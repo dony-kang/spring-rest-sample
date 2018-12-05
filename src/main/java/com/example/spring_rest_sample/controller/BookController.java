35ee0201782b1cc15650d677e17c7314a0f10b99
saytokang <saytokang@gmail.com>
2018-12-05 13:34:27 +0900

    swagger 적용한 샘플

--- src/main/java/com/example/spring_rest_sample/controller/BookController.java
+++ src/main/java/com/example/spring_rest_sample/controller/BookController.java
@@ -16,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(description = "RESTFull webservice")
 @RestController
 @RequestMapping("/rest")
 public class BookController {
@@ -23,6 +28,7 @@ public class BookController {
     @Autowired
     private BookDao bookDao;
 
+    @ApiOperation(value = "모든 책 리스트", response = List.class)
     @RequestMapping(value = "/books", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE })
     public List<Map<String, Object>> getBooks() {
@@ -30,9 +36,10 @@ public class BookController {
         return bookDao.findAll();
     }
 
+    @ApiOperation(value = "책 조회 by Id")
     @RequestMapping(value = "/books/{id}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE })
-    public Optional<Book> getBook(@PathVariable Long id) {
+    public Optional<Book> getBook(@ApiParam("Id of the book") @PathVariable Long id) {
         Optional<Book> book = bookDao.findById(id);
         if (!book.isPresent())
             throw new BookNotFoundException("id : " + id);
@@ -40,11 +47,13 @@ public class BookController {
         return book;
     }
 
+    @ApiOperation(value = "책 삭제 by Id")
     @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
-    public void deleteBook(@PathVariable Long id) {
+    public void deleteBook(@ApiParam("Id of the book") @PathVariable Long id) {
         bookDao.delete(id);
     }
 
+    @ApiOperation(value = "책 수정. by Id")
     @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Object> updateBook(@RequestBody Book book, @PathVariable Long id) {
         book.setId(id);
